33c40d984c5fc7cc5d7a9e3ca2875241a093e655
stijnie2210 <stijnmommersteeg@gmail.com>
2018-01-11 22:35:12 +0100

    Kan 2 characterkaarten drawen

--- Machiavelli/phases/GamePhase.cpp
+++ Machiavelli/phases/GamePhase.cpp
@@ -20,27 +20,51 @@ namespace machiavelli
 	void GamePhase::entered_phase(const Socket & socket, const Player & player)
 	{
 		socket << "Welcome to the GamePhase!\r\n";
+		
 	}
 
 	void GamePhase::add_options()
 	{
-		auto game = state()->game();
+		auto& game = state()->game();
 
-		if (game.current_player()->get_player() == game.getKing()) {
-			add_option("Pak 2 character kaarten", "character card command", std::bind(&GamePhase::handle_character_cards, this, _1, _2), true);
+		if (!characterCardsDrawn) {
+			if (game.current_player()->get_player() == game.getKing()) {
+				add_option("0", "Pak 2 character kaarten", std::bind(&GamePhase::handle_character_cards, this, _1, _2), true);
+			}
 		}
 	}
 
 	void GamePhase::handle_character_cards(const Socket & socket, Player & player)
 	{
-		auto game = state()->game();
-		std::vector<CharacterCard> drawnCards = game.drawAmountOfCharacterCards(2);
+		auto& game = state()->game();
+		game.shuffleCharacterCards();
+		CharacterCard drawnCard = game.drawCharacterCard();
 
-		socket << "Je hebt deze twee kaarten getrokken: " << drawnCards.at(0).name() << " en " << drawnCards.at(1).name();
+		if (drawnCard.name() != "<no name>") {
+			socket << "Je hebt deze kaart getrokken: " << drawnCard.name() <<  "\r\n";
+			socket << "Deze kaart wordt gedekt op tafel gelegd." << "\r\n";
+			game.addCardtoTableDeck(drawnCard);
+			characterCardsDrawn = true;
+		}
 
-		add_option("0", "Leg een character kaart af", [=](const auto& a, auto& b) {
+		reset_options(true);
 
-			reset_options(false);
+		add_option("0", "Pak een character kaart", [=](const auto& a, auto& b) {
+			auto& game = state()->game();
+
+			reset_options(true);
+
+			auto characterCard = game.drawCharacterCard();
+
+			if (characterCard.name() != "<no name>") {
+				a << "Je hebt deze kaart getrokken: " << characterCard.name() << "\r\n";
+				a << "Deze kaart krijg je in je hand. De stapel wordt nu doorgegeven." << "\r\n";
+				b.addCharacterCardToDeck(game.drawCharacterCard());
+			}
+
+			print_info(a, b);
 		}, true);
+
+		print_info(socket, player);
 	}
 }
