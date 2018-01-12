6777c252a8e8caee0a8e1aae2d01d1ed0b7d1f49
Tom van Nimwegen <tomvannimwegen@gmail.com>
2018-01-12 11:11:54 +0100

    guard

--- Machiavelli/Player.cpp
+++ Machiavelli/Player.cpp
@@ -186,6 +186,9 @@ namespace machiavelli {
 
 	void Player::destroy_building(const std::string & name)
 	{
+		if (name == "Kerker")
+			return;
+
 		for (auto & card : building_cards) {
 			if(card.name() == name)
 				card.setIsBuilt(false);
